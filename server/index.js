ddb0159119cc8843403bedd31711687cd361ef21
Christopher-Yoon <pofour@fastlane.kr>
2025-05-08 12:56:38 +0900

    fix: api

---
+++ server/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.SERVICE_PORT
+
+app.get('/', (req, res) => {
+    res.send('수유실 위치 API 서버입니다.');
+});
+
+app.listen(PORT, () => {
+    console.log('서버 실행중 on port ${PORT}');
+});
